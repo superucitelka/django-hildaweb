1c1456b5af7fad939c70ac40d72c5dc00adccccf
unknown <dokonala.hilda@gmail.com>
2020-04-13 22:50:26 +0200

    Vytvoření nové aplikace, základní konfigurace, URL mapování

--- hildaweb/settings.py
+++ hildaweb/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'movies',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'cs'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Prague'
 
 USE_I18N = True
 
---
+++ movies/urls.py
@@ -0,0 +1,9 @@
+# Import modulu path z balíku django.urls
+from django.urls import path
+# Import modulu views (soubor views.py) ze stejné složky (.)
+from . import views
+
+# URL mapování - seznam URL adres pro aplikaci movies
+urlpatterns = [
+
+]
